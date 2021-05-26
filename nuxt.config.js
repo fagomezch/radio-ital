ef279dc6da8bc7bca25d40a557e2d30ac0667f10
fagomezch <fagomezch@gmail.com>
2021-05-26 17:27:25 -0500

    RI-7 Add global styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,5 +52,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend (config, { loaders }) {
+      loaders.scss.additionalData = '@use "sass:math";'
+    }
   }
 }
